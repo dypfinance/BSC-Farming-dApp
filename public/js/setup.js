d6e526da7ff2f273f8e9d215709fe4f740e62cd0
doyledyp <doyledev@protonmail.com>
2021-10-14 17:25:00 +0100

    Update Connect wallet

--- public/js/setup.js
+++ public/js/setup.js
@@ -3644,7 +3644,7 @@ async function connectWallet(provider, walletType) {
 		//Web3 Providers
 			window.web3 = new Web3(window.ethereum)
 		try {
-			//await window.ethereum.enable()
+			await window.ethereum.enable()
 			console.log("Connected!")
 			window.IS_CONNECTED = true
 			if ( window.ethereum.isCoin98 )
